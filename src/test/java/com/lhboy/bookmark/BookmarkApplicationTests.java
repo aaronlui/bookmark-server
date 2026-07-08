4d87f2049452670a943353aaeb9ff4bec234948c
Aaron Lui <aaron_lui@163.com>
2026-07-08 16:05:05 +0800

    first commit

---
+++ src/test/java/com/lhboy/bookmark/BookmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhboy.bookmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
